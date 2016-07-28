a36527d97e83c2d8b4d3555afb64fac05ec46e3a
giangnt6988 <giangnt6988@gmail.com>
2016-07-28 11:17:32 +0700

    edit search page

--- wp-content/themes/kos/search.php
+++ wp-content/themes/kos/search.php
@@ -8,7 +8,23 @@
  */
 
 get_header(); ?>
-
+<div class="breadcrumbs-box clearfix">
+    <div class="container">
+        <div class="breadcrumbs">
+            <?php if ( function_exists('yoast_breadcrumb') ) {
+                $breadcrumbs = yoast_breadcrumb('<span itemscope itemtype="http://data-vocabulary.org/Breadcrumb">','</span>',false);
+                echo $breadcrumbs;
+            } ?>
+        </div>
+        <div class="pull-right">
+            <div id="plusone-div" class="plusone"></div>
+            <script type="text/javascript">
+                gapi.plusone.render('plusone-div',{"size": "medium", "count": "true"});
+            </script>
+        </div>
+    </div>
+</div>
+<div class="container">
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -19,29 +35,40 @@ get_header(); ?>
 				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'kos' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			<div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="list-game-default clearfix">
+                        <?php
+                        /* Start the Loop */
+                        while ( have_posts() ) : the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+                            /*
+                             * Include the Post-Format-specific template for the content.
+                             * If you want to override this in a child theme, then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+                            if(!is_single( $post )) {
+                                get_template_part( 'template-parts/content-common', get_post_format() );
+                            }
+                            else {
+                                get_template_part( 'template-parts/content', get_post_format() );
+                            }
 
-			endwhile;
+                        endwhile;
 
-			the_posts_navigation();
 
-		else :
+                        else :
 
-			get_template_part( 'template-parts/content', 'none' );
+                            get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
-
+                        endif; ?>
+                    </div>
+                    <?php numeric_posts_nav(); ?>
+                </div>
+            </div>
 		</main><!-- #main -->
 	</section><!-- #primary -->
+</div>
 
 <?php
 get_sidebar();
--- wp-content/themes/kos/template-parts/content-search.php
+++ wp-content/themes/kos/template-parts/content-search.php
@@ -9,22 +9,19 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php kos_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php kos_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+<article id="post-<?php the_ID(); ?>" <?php post_class('game-item'); ?>>
+	<div class="inner">
+		<div class="entry-content">
+			<a class="image img-4x3" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				<img src="<?php echo $post->game_image; ?>" alt="<?php the_title(); ?>">
+			</a>
+		</div><!-- .entry-content -->
+		<header class="entry-header">
+			<?php
+				the_title( '<h4 class="entry-title game-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );
+			?>
+			<span class="squre-view">81</span>
+			<span class="count-played"><i class="icon-cm icon-cm-game-pad-mini"></i> <?php echo $post->game_views ?></span>
+		</header><!-- .entry-header -->
+	</div>
 </article><!-- #post-## -->
